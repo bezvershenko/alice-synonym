14641f072890a0a2392cefcffdb4781877a66f95
Andrew <saintruler@gmail.com>
2018-05-14 17:48:06 +0300

    change from syn to tr
    Signed-off-by: Andrew <saintruler@gmail.com>

--- synonyms/__init__.py
+++ synonyms/__init__.py
@@ -7,9 +7,10 @@ def get_synonym(text):
     try:
         request = requests.get('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?', params={
             'key': SYNONYM_API, 'lang': 'ru-ru', 'text': text
-        }).json()['def'][0]['tr'][0]['syn']
+        }).json()['def'][0]['tr']
         return 'Найден синоним: ' + requests.get('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?', params={
             'key': SYNONYM_API, 'lang': 'ru-ru', 'text': text
-        }).json()['def'][0]['tr'][0]['syn'][random.randrange(len(request))]['text']
+        }).json()['def'][0]['tr'][random.randrange(len(request))]['text']
     except:
         return 'Синоним не найден'
+
